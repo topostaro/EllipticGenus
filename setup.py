dc5827413a8a4ecbdf931da5e09d837238c6baf5
Kenta Kobayashi <kenta.topos@gmail.com>
2023-06-11 17:18:38 +0900

    set version to 1.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="EllipticGenus",
-    version="0.0.1",
+    version="1.0.0",
     author="Kenta Kobayashi, Akihito Nakamura, Kazushi Ueda",
     author_email="kenta.topos@gmail.com",
     license="GPL2+",
